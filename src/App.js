bba8fa4ffc1ee101e2b377076233fc8576a73195
stephen shadrach <stephenlgz@gmail.com>
2022-11-15 09:43:48 -0500

    firebase + states setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// styles and components
 import "./App.css";
 import Header from "./components/Header";
 import CreateUserPage from "./pages/CreateUser";
 import LoginPage from "./pages/Login";
 import UserProfilePage from "./pages/UserProfile";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC3Q5pnYHUbC31jxOhoMj68PAp9qv0T7aY",
+  authDomain: "dynamic-web-exercise-six.firebaseapp.com",
+  projectId: "dynamic-web-exercise-six",
+  storageBucket: "dynamic-web-exercise-six.appspot.com",
+  messagingSenderId: "798945789087",
+  appId: "1:798945789087:web:332e0540ee89122ecd4eaa",
+};
 
 const router = createBrowserRouter([
   {
@@ -21,6 +34,44 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const [appInitialized, setAppInitialized] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInformation, setUserInformation] = useState({});
+
+  useEffect(() => {
+    if (appInitialized) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // user is signed in
+          setUserInformation(user);
+          setIsLoggedIn(true);
+        } else {
+          // user is signed out
+          setUserInformation({});
+          setIsLoggedIn(false);
+        }
+        setIsLoading(false);
+      });
+    }
+  }, [appInitialized]);
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    initializeApp(firebaseConfig);
+    setAppInitialized(true);
+  }, []);
+
   return (
     <div className="App">
       <Header />
